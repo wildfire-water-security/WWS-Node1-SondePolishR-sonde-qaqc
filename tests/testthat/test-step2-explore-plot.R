4c4f63c38b588d4c14086c60b97cf2ae3dd0bb59
katiewampler <katie.wampler@oregonstate.edu>
2026-02-23 15:52:54 -0800

    adding tests
    forgot to commit in previous push

--- tests/testthat/test-step2-explore-plot.R
+++ tests/testthat/test-step2-explore-plot.R
@@ -20,12 +20,66 @@ test_that("module sever 2 works", {
     #absolute min and max of df
     expect_equal(date_bounds(), list(min=as.Date(min(raw_sonde$Date_MM_DD_YYYY)), max= as.Date(max(raw_sonde$Date_MM_DD_YYYY))))
 
-
-  #could use some shinytest2 to check flipping week
-
   },
   #these are passed to the module
   args = list(
     data = reactive({data_prj}) #expected that data is a reactive value so need to pass it that
   ))
 })
+
+
+library(shinytest2)
+
+test_that("{shinytest2} recording: checking-module2", {
+  # Don't run these tests on the CRAN build servers
+  skip_on_cran()
+
+  #checking the week selection works
+  local_app_support(test_path("../../inst/app"))
+  app <- AppDriver$new(test_path("../../inst/app"), variant = platform_variant(),
+                       name = "checking-module2", height = 911, width = 1619)
+  app$upload_file(`data1-file` = file.path(test_path(), "testdata/example-sonde-project.RDS"))
+
+  #check initial plot is made
+  app$expect_values()
+  plot_obj <- app$get_value(export = "data2-plot_obj")
+  vdiffr::expect_doppelganger("initial plot is made", plot_obj)
+
+  #check putting in week view
+  app$set_inputs(`data2-week_view` = TRUE)
+  plot_obj <- app$get_value(export = "data2-plot_obj")
+  vdiffr::expect_doppelganger("week view works", plot_obj)
+
+  #clicking next week
+  app$click("data2-next_week")
+  rng <- app$get_value(input= "data2-date_range")
+  expect_equal(rng, as.Date(c("2024-04-29", "2024-05-06")))
+
+  #click previous week
+  app$click("data2-prev_week")
+  rng <- app$get_value(input= "data2-date_range")
+  expect_equal(rng, as.Date(c("2024-04-22", "2024-04-29")))
+
+  #unclick weekly and make sure we get the full plot again
+  app$set_inputs(`data2-week_view` = FALSE)
+  plot_obj <- app$get_value(export = "data2-plot_obj")
+  vdiffr::expect_doppelganger("removing week view works", plot_obj)
+  rng <- app$get_value(input= "data2-date_range")
+  expect_equal(rng, as.Date(c("2024-04-22", "2024-05-15")))
+
+  #check changing variable to plot
+  app$set_inputs(`data2-update_parms-y_var` = "fDOM_RFU")
+  plot_obj <- app$get_value(export = "data2-plot_obj")
+  vdiffr::expect_doppelganger("changing plot variable", plot_obj)
+
+  #check on the table
+  tab <- app$get_value(export = "data2-log_table")
+  expect_true(nrow(tab) == 3)
+
+  #try with other df
+  app$upload_file(`data1-file` = file.path(test_path(), "testdata/sonde-example.csv"))
+  tab <- app$get_value(export = "data2-log_table")
+  expect_true(nrow(tab) == 1)
+
+})
+
