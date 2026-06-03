d9c51f070d348e1092838df91de5d8fd3688d812
Katie Wampler <katie.wampler@oregonstate.edu>
2026-06-03 12:57:02 -0700

    working on how to deal with duplicate values in app
    -I think the workflow pretty much works, I would like the row selection to be selecting a row in the df or something, but I'm stuck on how to deal with the version control here...

--- dev/testing-workflow-mod3.R
+++ dev/testing-workflow-mod3.R
@@ -81,3 +81,63 @@
 
   #flags and updates data
   #apply_edit_server("apply_limits", sondeproj, edit)
+
+#allowing user to deal with duplicates -----
+  #getting messy data to test workflow
+  proj <- readRDS(file.path(test_path(), "testdata", "example-sondeproj-messy.RDS"))
+  data <- proj$data
+  data$fDOM_QSU[data$FileName == "dupfile2.csv"] <- data$fDOM_QSU[data$FileName == "dupfile2.csv"] * 1.1
+
+
+  #identify duplicates
+  dups <- identify_dups(data)
+
+  row <- 2
+  y_var <- "fDOM_QSU"
+  #for each row user can choose to keep v1, v2, or average values
+    #show plot of that duplicated region to help determine which to keep
+    plot_dat <- data %>% filter(.data$DateTime_rd >= dups$start[row] & .data$DateTime_rd <= dups$end[row]) %>%
+      rename("duplicate_id" = "FileName")
+
+  if(dups$duptype[row] == "same file"){
+    plot_dat <- plot_dat %>% group_by(.data$DateTime_rd) %>% mutate(duplicate_id = paste("set", row_number())) %>%
+      ungroup() }
+
+  ggplot(plot_dat, aes(x=.data$DateTime_rd, y=.data[[y_var]], color=duplicate_id)) + geom_line() + geom_point() +
+      labs(color = "duplicate ID")
+
+  #user selects ID
+  keep_opts <- c(unique(plot_dat$duplicate_id), "use_mean")
+
+  keep_opts <- "use_mean"
+  #work through what would happen
+  if(keep_opts  == "use_mean"){
+    #pull out data we want to summarise
+    data_rm <- data %>% filter(!(.data$DateTime_rd >= dups$start[row] & .data$DateTime_rd <= dups$end[row]))
+
+    #summarise data
+    df_sum <-  data %>% filter(.data$DateTime_rd >= dups$start[row] & .data$DateTime_rd <= dups$end[row]) %>%
+      group_by(Date, DateTime_rd) %>%
+      summarise(across(everything(), ~ {
+        if(is.character(.)) {paste(unique(.), collapse = ";")
+        }else if (inherits(., c("Date", "POSIXct", "POSIXlt"))) {
+          as.POSIXct(mean(as.numeric(.), na.rm = TRUE), origin = "1970-01-01")
+        }else if (is.numeric(.)) {
+          mean(., na.rm = TRUE)
+        }else{NA}}), .groups = "drop_last")
+
+    data_nodup <- data_rm %>% bind_rows(df_sum) %>% arrange(.data$DateTime_rd) %>%
+      mutate(Index = 1:n())
+
+    data_nodup
+  }else{
+    rm_index <- plot_dat$Index[plot_dat$duplicate_id != keep_opts]
+    data_nodup <- data[-rm_index]
+    data_nodup
+  }
+
+  #when they do this, it creates an edit log that this was done --> problem because current version control won't work with dupes
+    #issue: how to we track this change, because when new data is added, we'll lose the current dup list if they're removed
+      #best I got right now is that the flagging functions will look for duplicates, and if there are duplicates it handles it differently
+        #but not sure exactly what that looks like yet....
+  #
